6317de613a5f5328b091ec0e60508f7de0b0fc4e
Robin Koning <robin.t.koning@gmail.com>
2018-01-16 12:49:10 +0100

    Added comments

--- nengo/cats.py
+++ nengo/cats.py
@@ -35,20 +35,30 @@ def input_to_vision(t):
 #model definition: uses spa.SPA because we use semantic pointers
 model = spa.SPA()
 with model:
-
+    
+    # Create the vision and motor states.
     model.vision = spa.State(D)
     model.motor = spa.State(D)
     
-    #spa Input node that presents cat, tiger, or nothing, based on input_to_vision
+    # spa Input node that presents cat, tiger, or nothing, based on 
+    # input_to_vision
     model.stim = spa.Input(vision=input_to_vision) 
     
+    # Define the actions. 
+    # If vision holds the CAT state --> PET
+    # If vision holds the TIGER state --> RUN
+    # If both fire equally --> STARE
     actions = spa.Actions(
         'dot(vision, CAT) --> motor=PET',
         'dot(vision, TIGER) --> motor=RUN',
         '0.5 --> motor=STARE'
         )
     
+    # Create BasalGanglia for motorcontrol and give it the actions defined 
+    # above.
     model.bg = spa.BasalGanglia(actions)
+    # Create Thalamus for relaying motor control signals from BG to motor 
+    # module.
     model.thal = spa.Thalamus(model.bg)
 
 
--- nengo/part_a.py
+++ nengo/part_a.py
@@ -5,14 +5,26 @@ def centered_square(x):
 
 model = nengo.Network()
 with model:
+    # We want 100 neurons in the same layer.
     n = 100
     d = 1
+    
+    # Add an input node.
     stimulus = nengo.Node([0])
+    
+    # Create layers.
     a = nengo.Ensemble(n_neurons=n, dimensions=d)
     b = nengo.Ensemble(n_neurons=n, dimensions=d)
     c = nengo.Ensemble(n_neurons=n, dimensions=d)
     
+    # Connect first layer to the input.
     nengo.Connection(stimulus, a)
+    
+    # Connect layer A to B, passing the square centered around 0 
+    # of layer A's input to B.
     nengo.Connection(a, b, function=centered_square)
+    # Connect layer B to layer C, layer B passes half its input to C.
     nengo.Connection(b, c, transform=0.5)
+    # Connect C to itself, creating a memory layer. It passes whatever value it 
+    # currently holds to itself every 100ms.
     nengo.Connection(c, c, synapse=0.1)
